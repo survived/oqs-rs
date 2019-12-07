ad83d52eac75aa36a71b005a9e748065ec42a4ca
Denis <denis.varlakov@vdomresearch.com>
2019-12-07 19:09:13 +0700

    Fix dynamic linking during the oqs-sys building

--- oqs-sys/build.rs
+++ oqs-sys/build.rs
@@ -31,7 +31,7 @@ fn main() {
 
     println!(
         "cargo:rustc-link-search=native={}",
-        oqs_dir.to_string_lossy()
+        oqs_dir.join(".libs").to_string_lossy()
     );
 
 //    let _ = bindgen::builder()
